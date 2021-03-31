c6eb840434b3bbbbe8da100b7a19f0183dbabcc1
Joel Fintan <joelfintan@gmail.com>
2021-03-31 14:14:49 +0530

    Added console.log - Extra commit line 1 - Extra commit line 2 - Extra commit line 3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+console.log("Hello")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
